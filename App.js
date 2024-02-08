b14950f4e616bb7a06ce4c3f59334ed42a2b2bfd
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-08 10:53:19 -0300

    Criação dos estilos

--- App.js
+++ App.js
@@ -28,4 +28,34 @@ export default function App() {
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewLogo: {
+    backgroundColor: "green",
+    flex: 3,
+    width: "80%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  viewBotoes: {
+    backgroundColor: "orange",
+    flex: 2,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    width: "80%",
+    justifyContent: "space-evenly",
+  },
+  viewRodape: {
+    width: "80%",
+    backgroundColor: "red",
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
