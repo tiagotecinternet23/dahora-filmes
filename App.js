f224378dd30ede86ba6b698552c91a12932bd56d
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-19 09:48:09 -0300

    Componente SafeContainer

--- App.js
+++ App.js
@@ -10,40 +10,13 @@ import {
 } from "react-native";
 import logo from "./assets/images/logo.png";
 import { Ionicons } from "@expo/vector-icons";
-
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
-
-/* Manter a tela splash visível enquanto não
-programarmos a ação de ocultar */
-SplashScreen.preventAutoHideAsync();
+import SafeContainer from "./src/components/SafeContainer";
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    "Monoton-Regular": require("./assets/fonts/Monoton-Regular.ttf"),
-    NotoSans: require("./assets/fonts/NotoSans-VariableFont.ttf"),
-  });
-
-  /* Função atrelada ao hook useCallback.
-  Quando uma função está conectada ao useCallback, garantimos
-  que a referência dela é armazenada na memória somente uma vez. */
-  const aoAtualizarLayout = useCallback(async () => {
-    /* Se estiver tudo ok com o carregamento */
-    if (fontsLoaded || fontError) {
-      /* Escondemos a splashscreen */
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView style={estilos.container} onLayout={aoAtualizarLayout}>
+      <SafeContainer>
         <View style={estilos.viewLogo}>
           <Image source={logo} style={estilos.logo} />
           <Text style={estilos.titulo}>Dá Hora Filmes</Text>
@@ -76,18 +49,12 @@ export default function App() {
             </Text>
           </Pressable>
         </View>
-      </SafeAreaView>
+      </SafeContainer>
     </>
   );
 }
 
 const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   viewLogo: {
     flex: 3,
     width: "80%",
