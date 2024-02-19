2cef8ea65f4c2e509901dc7489beff72d97976e2
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-19 10:30:39 -0300

    Tela Home

--- App.js
+++ App.js
@@ -1,101 +1,11 @@
-import {
-  Button,
-  Image,
-  Pressable,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import logo from "./assets/images/logo.png";
-import { Ionicons } from "@expo/vector-icons";
-import SafeContainer from "./src/components/SafeContainer";
+import { StatusBar } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeContainer>
-        <View style={estilos.viewLogo}>
-          <Image source={logo} style={estilos.logo} />
-          <Text style={estilos.titulo}>Dá Hora Filmes</Text>
-        </View>
-
-        <View style={estilos.viewBotoes}>
-          <Pressable style={estilos.botao}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="search" size={12} color="white" /> Buscar Filmes
-            </Text>
-          </Pressable>
-          <Pressable style={estilos.botao}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="star" size={12} color="gold" /> Favoritos
-            </Text>
-          </Pressable>
-        </View>
-
-        <View style={estilos.viewRodape}>
-          <Pressable style={estilos.botaoRodape}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="lock-closed" size={16} color="white" />{" "}
-              Privacidade
-            </Text>
-          </Pressable>
-          <Pressable style={estilos.botaoRodape}>
-            <Text style={estilos.textoBotao}>
-              <Ionicons name="information-circle" size={16} color="white" />{" "}
-              Sobre
-            </Text>
-          </Pressable>
-        </View>
-      </SafeContainer>
+      <Home />
     </>
   );
 }
-
-const estilos = StyleSheet.create({
-  viewLogo: {
-    flex: 3,
-    width: "80%",
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  logo: {
-    width: 144,
-    height: 144,
-  },
-  titulo: {
-    fontFamily: "Monoton-Regular",
-    fontSize: 30,
-    color: "#5a51a6",
-  },
-  viewBotoes: {
-    flex: 2,
-    flexDirection: "row",
-    alignItems: "flex-start",
-    width: "80%",
-    justifyContent: "space-evenly",
-  },
-  botao: {
-    padding: 16,
-    backgroundColor: "#5451a6",
-  },
-  textoBotao: {
-    fontFamily: "NotoSans",
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  viewRodape: {
-    width: "100%",
-    backgroundColor: "#5451a6",
-    flex: 0.5,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  botaoRodape: {
-    padding: 16,
-  },
-});
