9906ddeeb5862110a8fd6ccecb49088efad72159
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-26 08:27:48 -0300

    Tela Privacidade

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
       {/* <Home /> */}
-      <Sobre />
+      {/* <Sobre /> */}
+      <Privacidade />
     </>
   );
 }
