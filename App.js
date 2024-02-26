f6daf7f8cb4e19e2ed865e3b40cd0963315a8d14
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-02-26 09:17:56 -0300

    Config Navigator e Stack

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
+
+// Criação/inicialização do mecanismo Stack
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Home /> */}
-      {/* <Sobre /> */}
-      <Privacidade />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Bueno" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
