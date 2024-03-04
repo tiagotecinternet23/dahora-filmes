ef724096b9ee0b0efda811193038cb4cea63f45b
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-04 12:03:18 -0300

    Update Resultados.js

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,13 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
 import SafeContainer from "../components/SafeContainer";
+import { api, apiKey } from "../services/api-moviedb";
+import { useEffect, useState } from "react";
 
 /* Prop route
 Prop especial e definida pelo React Navigation.
 Através dela que é possível acessar valores passados
 por meio de navegação entre telas. */
 export default function Resultados({ route }) {
+  /* State para gerenciar os resultados da busca na API */
+  const [resultados, setResultados] = useState([]);
+
   // Capturando o parâmetro filme vindo de BuscarFilmes
   const { filme } = route.params;
+
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get(`/search/movie`, {
+          params: {
+            include_adult: false,
+            language: "pt-BR",
+            query: filme,
+            api_key: apiKey,
+          },
+        });
+        console.log(resposta.data);
+      } catch (error) {
+        console.error("Deu ruim: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
 
   return (
     <SafeContainer>
