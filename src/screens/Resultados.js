977eda2852ba57957b95d57be0753d3334dd56c6
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-08 09:18:50 -0300

    FlatList básica

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import { api, apiKey } from "../services/api-moviedb";
 import { useEffect, useState } from "react";
@@ -25,7 +25,9 @@ export default function Resultados({ route }) {
             api_key: apiKey,
           },
         });
-        console.log(resposta.data.results);
+
+        /* Adicionando os resultados ao state */
+        setResultados(resposta.data.results);
       } catch (error) {
         console.error("Deu ruim: " + error.message);
       }
@@ -37,15 +39,29 @@ export default function Resultados({ route }) {
     <SafeContainer>
       <View style={estilos.subContainer}>
         <Text style={estilos.texto}>Você buscou por: {filme} </Text>
+        <View style={estilos.viewFilmes}>
+          <FlatList
+            // Prop data apontando para o state contendo os dados para a FlatList
+            data={resultados}
+            // Extraindo a chave/key de cada registro/item/filme único
+            keyExtractor={(item) => item.id}
+            // Prop que irá renderizar cada item/filme em um componente
+            renderItem={({ item }) => {
+              return <Text> {item.title}</Text>;
+            }}
+          />
+        </View>
       </View>
     </SafeContainer>
   );
 }
 
 const estilos = StyleSheet.create({
+  viewFilmes: { marginVertical: 8 },
   subContainer: {
     flex: 1,
     padding: 16,
+    width: "100%",
   },
   texto: {
     marginVertical: 8,
