63e5e5978a48477459f35f75b27ee5bd0915f81a
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-11 08:56:04 -0300

    Tela Detalhes (básica)

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
 import BuscarFilmes from "./src/screens/BuscarFilmes";
 import Resultados from "./src/screens/Resultados";
+import Detalhes from "./src/screens/Detalhes";
 
 // Criação/inicialização do mecanismo Stack
 const Stack = createNativeStackNavigator();
@@ -44,6 +45,7 @@ export default function App() {
           />
 
           <Stack.Screen name="Resultados" component={Resultados} />
+          <Stack.Screen name="Detalhes" component={Detalhes} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ src/screens/Detalhes.js
@@ -0,0 +1,12 @@
+// rnfs
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Detalhes() {
+  return (
+    <View>
+      <Text>Detalhes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
