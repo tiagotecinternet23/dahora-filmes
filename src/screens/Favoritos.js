5c7f2f481ef60e2e99849503a86cc407e780246d
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-14 11:29:01 -0300

    Update Favoritos.js

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -20,7 +20,12 @@ export default function Favoritos() {
   useEffect(() => {
     const carregarFavoritos = async () => {
       try {
+        /* Acessamos o storage criado previamente e guardamos
+        as strings de dados. */
         const dados = await AsyncStorage.getItem("@favoritosdahora");
+
+        /* Se houver dados, transformamos em objetos (JSON.parse)
+        e atualizamos o state com a lista de favoritos */
         if (dados) {
           setListaFavoritos(JSON.parse(dados));
         }
