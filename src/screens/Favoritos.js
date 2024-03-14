824a5b32de350a16194ef74871d158ed8127c388
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-14 11:26:01 -0300

    Carregamento dos favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,7 +1,40 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import SafeContainer from "../components/SafeContainer";
 
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default function Favoritos() {
+  /* State para registrar os dados carregados do storage */
+  const [listaFavoritos, setListaFavoritos] = useState([]);
+
+  /* useEffect será disparado assim que o usuário entrar
+  na tela Favoritos (portanto, somente uma vez) */
+  useEffect(() => {
+    const carregarFavoritos = async () => {
+      try {
+        const dados = await AsyncStorage.getItem("@favoritosdahora");
+        if (dados) {
+          setListaFavoritos(JSON.parse(dados));
+        }
+      } catch (error) {
+        console.error("Erro ao carregar os dados: " + error);
+        Alert.alert("Erro", "Erro ao carregar os dados.");
+      }
+    };
+    carregarFavoritos();
+  }, []);
+
+  // Log no state
+  console.log(listaFavoritos);
+
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
