3cef4819573c8aefb7225b530937e19e02403cef
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-03-14 10:01:07 -0300

    Verificando AsyncStorage e listaDeFilmes

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -2,6 +2,7 @@ import { Alert, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import imagemAlternativa from "../../assets/images/foto-alternativa.jpg";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function CardFilme({ filme }) {
   const { title, poster_path } = filme;
@@ -11,8 +12,17 @@ export default function CardFilme({ filme }) {
     //Alert.alert("Favoritos", "Salvando...");
 
     try {
-      /* 1) Verificar/carregar os favoritos armazenados no AsyncStorage */
-      /* 2) Verificar/criar uma lista de filmes favoritos (dados) */
+      /* 1) Verificar/carregar os favoritos armazenados no AsyncStorage.
+      Usamos o getItem do AsyncStorage para analisar se existe um armazenamento com o nome indicado (@favoritosdahora). Existindo, ele é carregado para
+      a const filmesFavoritos. Se não existir, será criado posteriormente. */
+      const filmesFavoritos = await AsyncStorage.getItem("@favoritosdahora");
+
+      /* 2) Verificar/criar uma lista de filmes favoritos (dados).
+      Se filmesFavoritos existir (ou seja, tem dados no storage), pegamos
+      estes dados (strings) e convertemos em objeto (JSON.parse). Caso contrário,
+      listaDeFilmes será um array vazio. */
+      const listaDeFilmes = filmesFavoritos ? JSON.parse(filmesFavoritos) : [];
+
       /* 3) Verificar se já tem algum filme na lista */
       /* 4) Se o filme não estiver na lista, então vamos colocá-lo */
       /* 5) Usamos o AsyncStorage para gravar no armazenamento offline do dispositivo */
